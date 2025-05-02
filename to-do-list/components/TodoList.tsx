4bd911a1d6c1fe896097b0a3ffbdc09672a5e97b
Jacob Wilkins <112595113+jacob-wilkins@users.noreply.github.com>
2025-05-02 14:08:29 -0400

    added the delete and add todo functionality. styling is very needed

--- to-do-list/components/TodoList.tsx
+++ to-do-list/components/TodoList.tsx
@@ -11,17 +11,9 @@ interface TodoListProps {
 export default function TodoList({ initialTodos }: TodoListProps) {
     const [items, setItems] = useState<Todo[]>(initialTodos); // changing todos list
     const [editTitle, setEditTitle] = useState("");           // editing title of todo
+    const [newTitle, setNewTitle] = useState("");           // creating a new todo
     const [editModal, setEditModal] = useState<Todo | null>(null);  // for the popup modal
 
-    // changes state of the checkbox nex to each entry
-    const toggle = (id: number) => {
-      setItems((prev) =>
-        prev.map((t) =>
-          t.id === id ? { ...t, completed: !t.completed } : t
-        )
-      );
-    };
-    
     // makes the edit modal visible
     const openModal = (todo: Todo) => {
       setEditModal(todo);
@@ -34,8 +26,67 @@ export default function TodoList({ initialTodos }: TodoListProps) {
       setEditTitle("");
     }
 
+    // changes state of the checkbox nex to each entry
+    const toggle = (id: number) => {
+      setItems((prev) =>
+        prev.map((item) =>
+          item.id === id ? { ...item, completed: !item.completed } : item
+        )
+      );
+    };
+
+    // removes todo from the list
+    const remove = (id: number) => {
+      setItems((prev) => prev.filter((item) => item.id !== id));
+    };
+
+    const saveEdit = () => {
+      // only when the editing modal is open
+      if (editModal) {
+        setItems((prev) =>
+          prev.map((item) =>
+            // look in the todos array for the matching todo item
+            // when found, change the title to the editTitle variable
+            item.id === editModal.id ? { ...item, title: editTitle } : item
+          )
+        );
+        closeModal();
+      }
+    };
+
+    // creates a new todo list item
+    const createTodo = () => {
+      // if there is nothing in the text box when clicked
+      if (!newTitle.trim()) return;
+
+      // create the actual object
+      const newTodo: Todo = {
+        userId: 1,
+        id: Date.now(),
+        title: newTitle,
+        completed: false
+      }
+
+      // put it into the list
+      setItems((prev) => [...prev, newTodo]);
+
+      // make title empty again so the text field is reset
+      setNewTitle("");
+    }
+
     return (
       <>
+      <div>
+          <input 
+            type="text"
+            id="newTodo"
+            value={newTitle}
+            placeholder="Add a new todo item..."
+            onChange={(e) => setNewTitle(e.target.value)}
+            >
+          </input>
+          <button onClick={createTodo}>Create</button>
+      </div>
       <ul className="space-y-2">
         {items.map((todo) => (
           <li key={todo.id} className="flex items-center gap-2 w-full p-2 mb-4 bg-gray-700 text-white rounded">
@@ -46,7 +97,7 @@ export default function TodoList({ initialTodos }: TodoListProps) {
             />
             <span>{todo.title}</span>
             <button onClick={() => openModal(todo)}>Edit</button>
-            <button>Delete</button>
+            <button onClick={() => remove(todo.id)}>Delete</button>
           </li>
         ))}
       </ul>
@@ -71,7 +122,7 @@ export default function TodoList({ initialTodos }: TodoListProps) {
                 Cancel
               </button>
               <button
-                // onClick={saveEdit}
+                onClick={saveEdit}
                 className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded"
               >
                 Save
